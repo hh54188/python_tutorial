a63f61e17b7fcbc22837843d17068d71bbc6a3d6
liguangyi <252438823@qq.com>
2018-04-22 23:39:19 +0800

    update

---
+++ Chapter08/parameter_01.py
@@ -0,0 +1,14 @@
+def fn1(name='liguangyi'):
+    print(name)
+
+
+fn1()
+fn1('mengxiao')
+
+
+def fn2(name='liguangyi', age='2'):
+    print(name + ': ' + age)
+
+
+fn2()
+fn2(age='22', name='mengxiao')
